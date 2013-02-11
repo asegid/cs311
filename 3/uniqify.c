8cb1fd37621062805419f70d2a094c0e07b9bcb9
Jordan Bayles <jangofett481@gmail.com>
2013-02-11 00:23:00 -0800

    Start work on Assignment 3

---
+++ 3/uniqify.c
@@ -0,0 +1,9 @@
+/*
+ *      Author:  Jordan Bayles (baylesj), baylesj@engr.orst.edu
+ *     Created:  02/11/2013 12:16:40 AM
+ *    Filename:  uniqify.c
+ *
+ * Description:  Implementation of functionality similar to ``uniq''
+ */
+
+
