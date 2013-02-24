c171509022374006492a620c3cf99facd9bfdb2f
Jordan Bayles <jangofett481@gmail.com>
2013-02-23 16:36:20 -0800

    Basic sieve working

---
+++ 4/bitset.c
@@ -0,0 +1,62 @@
+/*
+ *      Author:  Jordan Bayles (baylesj), baylesj@engr.orst.edu
+ *     Created:  02/22/2013 07:52:21 PM
+ *    Filename:  bitset.c
+ *
+ * Description:  Bitset function implementations, inspired by:
+ *               stackoverflow.com/questions/2633400/c-c-efficient-bit-array
+ */
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "bitset.h"
+
+uint64_t bindex (uint64_t idx) {
+	return (idx / CHUNK_SIZE);
+}
+
+uint64_t boffset (uint64_t idx) {
+	return (idx % CHUNK_SIZE);
+}
+
+/* Create and destroy bitsets */
+struct bitset *bitset_alloc (uint64_t n_bits) {
+	struct bitset *bs = malloc (sizeof (*bs));
+	bs->n_chunks = (n_bits / CHUNK_SIZE) + 1;
+	/* Calloc is faster than malloc + memset, better performance. */
+	bs->chunks = calloc (bs->n_chunks, sizeof (*bs->chunks));
+
+	//bs->chunks = malloc (sizeof (*bs->chunks) * bs->n_chunks);
+	///* Initialize to zero. Small allocs may have junk in them */
+	//memset(bs->chunks, 0, sizeof (bs->chunks));
+
+	return (bs);
+}
+
+void bitset_free (struct bitset *bs) {
+	free (bs->chunks);
+	free (bs);
+}
+
+/* Bit operations */
+void bit_set (struct bitset *bs, uint64_t idx) {
+	bs->chunks[bindex(idx)] |= 1 << (boffset (idx));
+}
+
+void bit_toggle (struct bitset *bs, uint64_t idx) {
+	if (bit_get (bs, idx)) {
+		bit_clear (bs, idx);
+	} else {
+		bit_set (bs, idx);
+	}
+}
+
+void bit_clear (struct bitset *bs, uint64_t idx) {
+	bs->chunks[bindex(idx)] &= ~(1 << (boffset (idx)));
+}
+
+uint64_t bit_get (struct bitset *bs, uint64_t idx) {
+	return (bs->chunks[bindex(idx)] & (1 << (boffset (idx))));
+}
+
---
+++ 4/sieve_of_atkin.c
@@ -0,0 +1,68 @@
+/*
+ *      Author:  Jordan Bayles (baylesj), baylesj@engr.orst.edu
+ *     Created:  02/22/2013 07:34:58 PM
+ *    Filename:  sieve_of_atkin.c
+ *
+ * Description:  Basic (non-threaded or forked) Sieve of Atkin
+ */
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "bitset.h"
+
+int main (int argc, char **argv)
+{
+	uint32_t limit = UINT32_MAX;
+	printf( "bit size of int: %d\n", (int) sizeof(uint32_t) * 8);
+	uint32_t sqrt_lim = sqrt (limit);
+	struct bitset *bs = bitset_alloc (limit);
+
+	/* Put in candidate primes w/ odd num of reps */
+	for (uint32_t x = 1; x <= sqrt_lim; ++x) {
+		uint64_t x_sq = x * x;
+		for (int y = 1; y <= sqrt_lim; ++y) {
+			uint64_t y_sq = y * y;
+			uint64_t n = 4 * x_sq + y_sq;
+
+			if ((n <= limit)
+			&& (((n % 12) == 1) || (((n % 12) == 5)))) {
+				bit_toggle(bs, n);
+			}
+
+			n -= x_sq;
+			if ((n <= limit) && ((n % 12) == 7)) {
+				bit_toggle(bs, n);
+			}
+
+			n -= 2 * y_sq;
+			if ((x > y) && (n <= limit) && ((n % 12) == 11)) {
+				bit_toggle(bs, n);
+			}
+		}
+	}
+
+	/* Eliminate composites */
+	for (uint32_t n = 5; n <= sqrt_lim; ++n) {
+		if (bit_get (bs, n)) {
+			uint32_t n_sq = n * n;
+			for (uint32_t k = n_sq; k < limit; k += n_sq) {
+				bit_clear (bs, k);
+			}
+		}
+	}
+
+	/* Print primes */
+	printf ("2, 3");
+	uint32_t cnt = 0;
+	for (uint32_t n = 5; n < limit; ++n) {
+		if (bit_get (bs, n)) {
+			printf (", %ud", n);
+			++cnt;
+		}
+	}
+
+	printf("count : %ud\n", cnt);
+}
