bf5c17c6f84e3bb98b6f1ab22166f9ea646d40a1
Jordan Bayles <jangofett481@gmail.com>
2013-02-27 11:44:12 -0800

    Debugging sieve, old version MUCH faster...

--- 4/support/old-rev-c171/sieve_of_atkin.c
+++ 4/support/old-rev-c171/sieve_of_atkin.c
@@ -16,18 +16,19 @@
 int main (int argc, char **argv)
 {
 	uint32_t limit = UINT32_MAX;
+	printf( "bit size of int: %d\n", (int) sizeof(uint32_t) * 8);
 	uint32_t sqrt_lim = sqrt (limit);
 	struct bitset *bs = bitset_alloc (limit);
 
 	/* Put in candidate primes w/ odd num of reps */
 	for (uint32_t x = 1; x <= sqrt_lim; ++x) {
 		uint64_t x_sq = x * x;
-		for (uint32_t y = 1; y <= sqrt_lim; ++y) {
+		for (int y = 1; y <= sqrt_lim; ++y) {
 			uint64_t y_sq = y * y;
-			int64_t n = 4 * x_sq + y_sq;
+			uint64_t n = 4 * x_sq + y_sq;
 
 			if ((n <= limit)
-			&& (((n % 12) == 1) || ((n % 12) == 5))) {
+			&& (((n % 12) == 1) || (((n % 12) == 5)))) {
 				bit_toggle(bs, n);
 			}
 
@@ -47,7 +48,7 @@ int main (int argc, char **argv)
 	for (uint32_t n = 5; n <= sqrt_lim; ++n) {
 		if (bit_get (bs, n)) {
 			uint32_t n_sq = n * n;
-			for (uint32_t k = n_sq; k <= limit; k += n_sq) {
+			for (uint32_t k = n_sq; k < limit; k += n_sq) {
 				bit_clear (bs, k);
 			}
 		}
