f7e6299a8f8cff54cb84a85ac2d666722bda5e61
Jordan Bayles <jangofett481@gmail.com>
2013-03-05 23:50:30 -0800

    Final for ASSIGNMENT 4

--- 4/turnin/thread.c
+++ 4/turnin/thread.c
@@ -1,55 +1,42 @@
 /*
  *      Author:  Jordan Bayles (baylesj), baylesj@engr.orst.edu
  *     Created:  02/22/2013 07:34:58 PM
- *    Filename:  fork.c
+ *    Filename:  thread.c
  *
- * Description:  Forked sieve of atkin + happy numbers
+ * Description:  Threaded sieve of atkin + happy numbers
  */
 
 #include <assert.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
 #include <math.h>
-#include <semaphore.h>
-#include <signal.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/times.h>
-#include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
 
 #include "bst.h"
 
 typedef uint32_t chunk_t;
 
-#define BMP_PATH "/baylesj_bitmap"
-#define SEM_PATH "/baylesj_semaphore"
 #define CHUNK_SIZE (sizeof (chunk_t) * 8)
-#define PROC_NUM 4
-#define CHUNKS_PER_SEMAPHORE 1024
+#define THREAD_NUM 4
+#define CHUNKS_PER_MUTEX 1024
 
 /* Lookup table for squares of 0-9 for happy finding */
 const int squares[] = { 0, 1, 4, 9, 16, 25, 36, 49, 64, 81 };
 
 /* Shared variables (global for easy access) */
-struct bitset *bs;
 uint32_t lim;
-pid_t procs[PROC_NUM];
-sem_t *sem;
 uint32_t sq_lim;
+struct bitset *bs;
 
 struct bitset {
-	char chunks_path[PATH_MAX];
-	char sems_path[PATH_MAX];
-	sem_t *sems;
+	pthread_mutex_t *mutices;
 	chunk_t *chunks;
 	uint64_t n_chunks;
 };
@@ -61,10 +48,10 @@ struct args {
 
 /* Function prototypes */
 
-/* Find the semaphore associated with index */
-uint64_t bsemaphore(uint64_t idx)
+/* Find the mutex associated with index */
+uint64_t bmutex(uint64_t idx)
 {
-	return (idx / (CHUNK_SIZE * CHUNKS_PER_SEMAPHORE));
+	return (idx / (CHUNK_SIZE * CHUNKS_PER_MUTEX));
 }
 
 /* Chunk corresponding to index returned */
@@ -85,53 +72,31 @@ uint64_t chunks_size(struct bitset * bs)
 }
 
 /* Create and destroy bitsets */
-struct bitset *bitset_alloc(uint64_t n_bits, char bits_path[],
-			    char sem_path[])
+struct bitset *bitset_alloc(uint64_t n_bits)
 {
-	struct bitset *bs = malloc(sizeof(*bs));
 	uint32_t i;
-	int share_fd;
-	int sem_fd;
-	uint64_t sem_size;
-	uint64_t num_sems;
+	uint32_t num_mutexes;
+	pthread_mutexattr_t attr;
+	struct bitset *bs = malloc(sizeof(*bs));
 
+	/* Initialize chunk memory */
 	assert(bs != NULL);
-	strcpy(bs->chunks_path, bits_path);
-	strcpy(bs->sems_path, sem_path);
 	bs->n_chunks = (n_bits / CHUNK_SIZE) + 1;
+	bs->chunks = calloc(bs->n_chunks, sizeof(*bs->chunks));
+	assert(bs->chunks != NULL);
 
-	/* Map memory */
-	shm_unlink(bits_path);
-	if ((share_fd =
-	     shm_open(bits_path, O_RDWR | O_CREAT,
-		      S_IRUSR | S_IWUSR)) == -1)
-		perror("shm_open");
-	if (ftruncate(share_fd, chunks_size(bs)) == -1)
-		perror("ftruncate");
-	if ((bs->chunks =
-	     mmap(NULL, chunks_size(bs), PROT_WRITE | PROT_READ,
-		  MAP_SHARED, share_fd, 0)) == MAP_FAILED)
-		perror("mmap");
-
-	/* Map semaphores */
-	shm_unlink(sem_path);
-	sem_size = (bs->n_chunks / CHUNKS_PER_SEMAPHORE) * sizeof(sem_t);
-	printf("sem_size = %lu\n", (unsigned long) sem_size);
-	if ((sem_fd =
-	     shm_open(SEM_PATH, O_CREAT | O_RDWR,
-		      S_IRUSR | S_IWUSR)) == -1)
-		perror("shm_open");
-	if (ftruncate(sem_fd, sem_size) == -1)
-		perror("ftruncate");
-	if ((bs->sems = mmap(NULL, sem_size, PROT_WRITE | PROT_READ,
-			     MAP_SHARED, sem_fd, 0)) == MAP_FAILED)
-		perror("map failed");
-
-	/* Initialize semaphores */
-	num_sems = (bs->n_chunks / CHUNKS_PER_SEMAPHORE);
-	for (i = 0; i < num_sems; ++i) {
-		if (sem_init(&(bs->sems[i]), 1, 1) == -1)
-			perror("sem_init");
+	/* Initialize mutexes */
+	bs->mutices =
+	    calloc((bs->n_chunks / CHUNKS_PER_MUTEX),
+		   sizeof(pthread_mutex_t));
+	assert(bs->mutices != NULL);
+	pthread_mutexattr_init(&attr);
+	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+	num_mutexes = (bs->n_chunks / CHUNKS_PER_MUTEX);
+	for (i = 0; i < num_mutexes; ++i) {
+		if (pthread_mutex_init(&(bs->mutices[i]), &attr) > 0) {
+			perror("pthread_mutex_init");
+		}
 	}
 
 	return (bs);
@@ -140,38 +105,38 @@ struct bitset *bitset_alloc(uint64_t n_bits, char bits_path[],
 void bitset_free(struct bitset *bs)
 {
 	if (bs != NULL) {
+		if (bs->mutices != NULL)
+			free(bs->mutices);
 		if (bs->chunks != NULL)
-			munmap(bs->chunks, chunks_size(bs));
-		shm_unlink(bs->chunks_path);
-		shm_unlink(bs->sems_path);
+			free(bs->chunks);
 		free(bs);
 	}
 }
 
 /* Bit operations */
-void bit_set(struct bitset *bs, uint64_t idx, bool use_sem)
+void bit_set(struct bitset *bs, uint64_t idx, bool use_mutex)
 {
 	assert(bs != NULL);
-	if (use_sem) {
-		if (sem_wait(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_wait");
+	if (use_mutex) {
+		if (pthread_mutex_lock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_lock");
 	}
 
 	bs->chunks[bindex(idx)] |= 1 << (boffset(idx));
 
-	if (use_sem) {
-		if (sem_post(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_post");
+	if (use_mutex) {
+		if (pthread_mutex_unlock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_unlock");
 	}
 }
 
-void bit_toggle(struct bitset *bs, uint64_t idx, bool use_sem)
+void bit_toggle(struct bitset *bs, uint64_t idx, bool use_mutex)
 {
 	assert(bs != NULL);
 
-	if (use_sem) {
-		if (sem_wait(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_wait");
+	if (use_mutex) {
+		if (pthread_mutex_lock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_lock");
 	}
 
 	if (bs->chunks[bindex(idx)] & (1 << (boffset(idx)))) {
@@ -180,56 +145,58 @@ void bit_toggle(struct bitset *bs, uint64_t idx, bool use_sem)
 		bs->chunks[bindex(idx)] |= 1 << (boffset(idx));
 	}
 
-	if (use_sem) {
-		if (sem_post(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_post");
+	if (use_mutex) {
+		if (pthread_mutex_unlock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_unlock");
 	}
 }
 
-void bit_clear(struct bitset *bs, uint64_t idx, bool use_sem)
+void bit_clear(struct bitset *bs, uint64_t idx, bool use_mutex)
 {
 	assert(bs != NULL);
-	if (use_sem) {
-		if (sem_wait(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_wait");
+
+	if (use_mutex) {
+		if (pthread_mutex_lock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_lock");
 	}
 
 	bs->chunks[bindex(idx)] &= ~(1 << (boffset(idx)));
 
-	if (use_sem) {
-		if (sem_post(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_post");
+	if (use_mutex) {
+		if (pthread_mutex_unlock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_unlock");
 	}
 }
 
-chunk_t bit_get(struct bitset *bs, uint64_t idx, bool use_sem)
+chunk_t bit_get(struct bitset *bs, uint64_t idx, bool use_mutex)
 {
 	chunk_t bit;
 	assert(bs != NULL);
 
-	if (use_sem) {
-		if (sem_wait(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_wait");
+	if (use_mutex) {
+		if (pthread_mutex_lock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_lock");
 	}
 
 	bit = (bs->chunks[bindex(idx)] & (1 << (boffset(idx))));
 
-	if (use_sem) {
-		if (sem_post(&(bs->sems[bsemaphore(idx)])) == -1)
-			perror("sem_post");
+	if (use_mutex) {
+		if (pthread_mutex_unlock(&(bs->mutices[bmutex(idx)])) != 0)
+			perror("pthread_mutex_unlock");
 	}
 	return (bit);
 }
 
 /* Sieve functions */
-void candidate_primes(struct args in)
+void *candidate_primes(void *arg)
 {
+	struct args *in = (struct args *) arg;
 	int64_t n;
 	uint64_t x, y;
 	uint64_t x_sq, y_sq;
 
 	/* Put in candidate primes w/ odd num of reps */
-	for (x = in.min; x <= in.max; ++x) {
+	for (x = in->min; x <= in->max; ++x) {
 		x_sq = x * x;
 		for (y = 1; y <= sq_lim; ++y) {
 			y_sq = y * y;
@@ -251,16 +218,19 @@ void candidate_primes(struct args in)
 			}
 		}
 	}
+
+	pthread_exit(NULL);
 }
 
-void eliminate_composites(struct args in)
+void *eliminate_composites(void *arg)
 {
+	struct args *in = (struct args *) arg;
 	uint64_t k;
 	uint64_t n;
 	uint64_t n_sq;
 
 	/* Eliminate composites */
-	for (n = in.min; n <= in.max; ++n) {
+	for (n = in->min; n <= in->max; ++n) {
 		if (bit_get(bs, n, true)) {
 			n_sq = n * n;
 			for (k = n_sq; k <= lim; k += n_sq) {
@@ -268,6 +238,8 @@ void eliminate_composites(struct args in)
 			}
 		}
 	}
+
+	pthread_exit(NULL);
 }
 
 /* Happy functions */
@@ -308,12 +280,13 @@ bool is_happy(uint32_t num)
 	return (happy);
 }
 
-void determine_happies(struct args in)
+void *determine_happies(void *arg)
 {
+	struct args *in = (struct args *) arg;
 	uint64_t i;
 
 	/* Remove primes that aren't happy */
-	for (i = in.min; i <= in.max; ++i) {
+	for (i = in->min; i <= in->max; ++i) {
 		/*
 		 * Note that each thread should only access separate pieces of
 		 * array, there should be zero overlap
@@ -324,38 +297,34 @@ void determine_happies(struct args in)
 			}
 		}
 	}
+
+	pthread_exit(NULL);
 }
 
-void fork_proc(uint32_t min, uint32_t max, void (*func) (struct args))
+void spawn_threads(pthread_attr_t * attr, uint32_t min,
+		   uint32_t max, void *(*func) (void *))
 {
 	struct args arg;
 	uint32_t i;
-	pid_t child;
-	uint32_t block = ((max - min) / PROC_NUM);
+	pthread_t threads[THREAD_NUM];
+	uint32_t block = ((max - min) / THREAD_NUM);
 
-	for (i = 0; i < PROC_NUM; ++i) {
+	for (i = 0; i < THREAD_NUM; ++i) {
 		arg.min = i * block + min;
 		/* Handle rounding errors by just rounding up on last thread */
-		if (i == (PROC_NUM - 1))
+		if (i == (THREAD_NUM - 1))
 			arg.max = max;
 		else
 			arg.max = ((i + 1) * block) - 1 + min;
 
-		switch (child = fork()) {
-		case -1:
-			perror("fork");
-			break;
-		case 0:
-			(*func) (arg);
-			_exit(EXIT_SUCCESS);
-		default:
-			procs[i] = child;
-			break;
-		}
+		if (pthread_create
+		    (&threads[i], attr, (*func), (void *) &arg) != 0)
+			perror("pthread_create");
 	}
 
-	for (i = 0; i < PROC_NUM; ++i) {
-		wait(NULL);
+	for (i = 0; i < THREAD_NUM; ++i) {
+		if (pthread_join(threads[i], NULL) != 0)
+			perror("pthread_join");
 	}
 }
 
@@ -384,52 +353,30 @@ void print_times(struct tms timer, char *str)
 			 timer.tms_cstime) / clocks_per_second);
 }
 
-void sig_handle(int signo)
-{
-	int sig = (signo == SIGINT) ? SIGINT : SIGQUIT;
-
-	for (uint32_t i = 0; i < PROC_NUM; ++i)
-		kill(procs[i], sig);
-
-	for (uint32_t i = 0; i < PROC_NUM; ++i)
-		wait(NULL);
-
-	exit(EXIT_FAILURE);
-}
-
 int main(int argc, char **argv)
 {
 	struct tms timer;
-	struct sigaction handler;
 
 	/* Initialize globals */
 	lim = UINT32_MAX;
 	sq_lim = (uint32_t) sqrt((double) lim);
 	printf("Max value: %lu\n", (unsigned long) lim);
-	bs = bitset_alloc(lim, BMP_PATH, SEM_PATH);
-
-	/* Install a signal handler */
-	handler.sa_handler = sig_handle;
-	handler.sa_flags = 0;
-	sigemptyset(&handler.sa_mask);
-	sigaction(SIGHUP, &handler, NULL);
-	sigaction(SIGINT, &handler, NULL);
-	sigaction(SIGQUIT, &handler, NULL);
+	bs = bitset_alloc(lim);
 
 	/* Run program */
 	bit_set(bs, 2, false);
 	bit_set(bs, 3, false);
 	printf("finding candidate primes...\n");
-	fork_proc(1, sq_lim, candidate_primes);
+	spawn_threads(NULL, 1, sq_lim, candidate_primes);
 	printf("eliminating composite results...\n");
-	fork_proc(5, lim, eliminate_composites);
+	spawn_threads(NULL, 5, lim, eliminate_composites);
 	if (times(&timer) == -1)
 		perror("times");
 	print_times(timer, "primes");
 	print_on(bs, "primes");
 
 	printf("finding happy number status...\n");
-	fork_proc(1, lim, determine_happies);
+	spawn_threads(NULL, 1, lim, determine_happies);
 	print_on(bs, "happy primes");
 
 	/* Cleanup */
